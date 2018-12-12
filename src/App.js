e541f2416923a2215778e373012c57a20319dd08
Jason Glover <jglover@HerculesGroup.local>
2018-12-12 17:05:59 -0500

    Login using auth0 is completed and user token are now stored in local storage. Next step determine if use is logged in

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 import Home from './components/Home'
 import Profile from './components/Profile'
+import Nav from './components/Nav'
+import Auth from './Auth/Auth';
+import Callback from './components/Callback';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.auth = new Auth(this.props.history)
+  }
   render() {
     return (
       <>
-        <Route path="/" exact component={Home} />
+        <Nav />
+        
+        <Route path="/" exact render={props => <Home auth={this.auth} {...props} /> } />
+        <Route path="/callback" render={props => <Callback auth={this.auth} {...props}  /> } />
         <Route path="/profile"  component={Profile} />
       </>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
 
 class Home extends Component{
+
     render(){
         return(
-            <div> Home </div>
+            <div>
+                 <h2> Home </h2>
+                <button onClick={this.props.auth.login}> Login </button>
+            </div>
         )
     }
 }
